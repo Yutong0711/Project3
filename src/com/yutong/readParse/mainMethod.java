319784ed1142c1ba1b7a93290f66c89388290c5d
XudongY <31521395+XudongY@users.noreply.github.com>
2017-09-25 18:49:13 -0400

    indivial

--- src/com/yutong/readParse/mainMethod.java
+++ src/com/yutong/readParse/mainMethod.java
@@ -1,6 +1,33 @@
 package com.yutong.readParse;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+class Indivdual {
+	String ID;
+	String Name;
+	String Gender;
+	Date Birthday;
+	int Age;
+	boolean Alive;
+	Date Death;
+	List<String> Child = new ArrayList<>();
+	ArrayList<String> Spouse;
+}
+
+class Family {
+	String ID;
+	Date Married;
+	Date Divorced;
+	String HuabandID;
+	String HusbandName;
+	String WIfeID;
+	String WIfeName;
+	String[] Children;
+}
 
 public class mainMethod {
 
