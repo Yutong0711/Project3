258b52280799e4ce9331c5f814098a8ff2730825
XudongY <31521395+XudongY@users.noreply.github.com>
2017-09-28 17:21:43 -0400

    test

--- src/com/yutong/readParse/mainMethod.java
+++ src/com/yutong/readParse/mainMethod.java
@@ -60,7 +60,7 @@ class Family {
 
 public class mainMethod {
     public static void print_arraylist_family(ArrayList<Family> tobedisplayed) {
-
+        System.out.println(" ");
         String[] columnNames = {"ID", "MARRIED", "DIVORCED", "HUSBAND ID", "HUSBAND NAME", "WIFE ID", "WIFE NAME", "CHILDREN"};
 
         System.out.format("%5s%5s%5s%30s%10s%5s%2s%5s%5s%10s%5s%13s%5s%5s%3s%10s%5s", "|", columnNames[0], "|", columnNames[1], "|", columnNames[2], "|", columnNames[3], "|", columnNames[4], "|", columnNames[5], "|", columnNames[6], "|", columnNames[7], "|");
@@ -72,6 +72,7 @@ public class mainMethod {
         System.out.println(" ");
         for (int i = 0; i < tobedisplayed.size(); i++) {
             Family obj = tobedisplayed.get(i);
+
             System.out.format("%5s%5s%5s%30s%10s%5s%5s%5s%5s%10s%5s%15s%5s%5s%5s%10s%5s", "|", obj.ID, "|", obj.Married, "|", obj.Divorced, "|", obj.HusbandID, "|", obj.HusbandName, "|", obj.WifeID, "|", obj.WifeName, "|", String.valueOf(obj.Children), "|");
             System.out.println();
         }
@@ -84,6 +85,8 @@ public class mainMethod {
     public static void print_arraylist(ArrayList<Indivdual> tobedisplayed) {
 
         String[] columnNames = {"ID", "NAME", "GENDER", "AGE", "ALIVE", "DEATH", "CHILD", "SPOUSE"};
+        //  Indivdual longest=tobedisplayed.get(0);
+
 
         System.out.format("%5s%5s%5s%30s%10s%5s%4s%5s%5s%10s%5s%15s%5s%5s%5s%10s%5s", "|", columnNames[0], "|", columnNames[1], "|", columnNames[2], "|", columnNames[3], "|", columnNames[4], "|", columnNames[5], "|", columnNames[6], "|", columnNames[7], "|");
         System.out.println(" ");
@@ -92,7 +95,12 @@ public class mainMethod {
         }
         System.out.println(" ");
         for (int i = 0; i < tobedisplayed.size(); i++) {
+
             Indivdual obj = tobedisplayed.get(i);
+            if(obj.Death == null)
+            {
+                obj.Alive=true;
+            }
             System.out.format("%5s%5s%5s%30s%10s%5s%5s%5s%5s%10s%5s%15s%5s%5s%5s%10s%5s", "|", obj.ID, "|", obj.Name, "|", obj.Gender, "|", obj.Age, "|", obj.Alive, "|", obj.Death, "|", String.valueOf(obj.Child), "|", String.valueOf(obj.Spouse), "|");
             System.out.println();
         }
